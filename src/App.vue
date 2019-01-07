56f34b3a4fbeb377f07d57b9f380a80ba00b148a
Philippe Ahles <philippe@ahles.ch>
2019-01-07 18:03:43 +0100

    vuetify aside

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,38 @@
 <template>
   <v-app>
-    <v-toolbar dark fixed dense app>
-      xpvuemap
+    <v-navigation-drawer
+      v-model="drawer"
+      right
+      app
+    >
+      <v-list two-line>
+        <v-list-tile avatar>
+          <v-icon large>flight</v-icon><span class="section-header">Position</span>
+        </v-list-tile>
+        <v-list-tile avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>Latitude</v-list-tile-title>
+            <v-list-tile-sub-title>0</v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>Longitude</v-list-tile-title>
+            <v-list-tile-sub-title>0</v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>Altitude</v-list-tile-title>
+            <v-list-tile-sub-title>0</v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark dense app>
+      <v-toolbar-title>xpvuemap</v-toolbar-title>
+      <v-spacer />
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"/>
     </v-toolbar>
     <v-content>
       <router-view/>
@@ -9,8 +40,20 @@
   </v-app>
 </template>
 
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    drawer: true,
+  }),
+};
+</script>
+
 <style lang="scss">
-// .v-toolbar {
-//   overflow: hidden;
-// }
+body {
+  overflow: hidden;
+}
+.section-header {
+  padding-left: 18px;
+}
 </style>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -18,7 +18,7 @@
       </tbody>
     </table>
     -->
-    <div class="altitude">Altitude: {{ Math.floor(position.altitude) }} ft</div>
+    <div v-if="altitude" class="altitude">Altitude: {{ Math.floor(altitude) }} ft</div>
     <div id="map"></div>
   </div>
 </template>
@@ -31,25 +31,44 @@ export default {
   name: 'Map',
   data: function () { /* eslint-disable-line */
     return {
-      position: {},
+      altitude: null,
+      latitude: null,
+      longitude: null,
       map: null,
       marker: null,
       socket: null,
     };
   },
+  computed: {
+    altitudeM() {
+      return this.altitude * 0.3048;
+    },
+  },
   mounted() {
-    this.map = L.map('map').setView([51.505, -0.09], 13);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(this.map);
-    this.marker = L.marker([51.5, -0.09]).addTo(this.map);
-
-    this.socket = io('http://localhost:3000');
-    this.socket.on('position', (position) => {
-      this.position = position;
-      this.map.setView([this.position.latitude, this.position.longitude]);
-      this.marker.setLatLng(L.latLng(this.position.latitude, this.position.longitude));
-    });
+    this.initializeMap();
+    this.listenToSocketandGetPosition();
+  },
+  methods: {
+    initializeMap() {
+      this.map = L.map('map').setView([51.505, -0.09], 10);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(this.map);
+      this.marker = L.marker([51.5, -0.09]).addTo(this.map);
+    },
+    listenToSocketandGetPosition() {
+      this.socket = io('http://localhost:3000');
+      this.socket.on('position', (position) => {
+        this.altitude = position.altitude;
+        this.latitude = position.latitude;
+        this.longitude = position.longitude;
+        this.positionMarkerAndSetMapView();
+      });
+    },
+    positionMarkerAndSetMapView() {
+      this.map.setView([this.latitude, this.longitude]);
+      this.marker.setLatLng(L.latLng(this.latitude, this.longitude));
+    },
   },
 };
 </script>
@@ -73,7 +92,7 @@ table {
   left: 0;
   right: 0;
   bottom: 0;
-  width: 100vw;
+  width: 100%;
   height: calc(100vh - 48px);
   z-index: 0;
 }
@@ -81,7 +100,7 @@ table {
   position: absolute;
   top: 20px;
   right: 20px;
-  z-index: 20;
+  z-index: 1;
   background: rgba(255,255,255,0.75);
   padding: 10px;
   border-radius: 10px;
