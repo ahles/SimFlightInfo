7aeab1074a117f9406c6773f6a9aa60db2783bb1
Philippe Ahles <philippe@ahles.ch>
2019-01-07 15:43:15 +0100

    electron running, server and client merged

--- README.md
+++ README.md
@@ -1,49 +1,25 @@
 # xpvuemap
 
-Work in progress
+A Electron-APP that listens to the position information sent by X-Plane (over UDP) and displays the current location on a map.
 
 ## Project setup
 ```
 yarn install
 ```
 
-## Run server
-
-The server receives (UDP), evaluates and forwards (socket.io) the information sent by X-Plane.
-
+## DEV Mode
 ```
-yarn run server
+yarn electron:serve
 ```
 
-## Run client
-
-At the moment, the client is a Vue.js development server that receives the X-Plane information (socket.io) and displays the map.
-
-
-### Compiles and hot-reloads for development
+## BUILD App
 ```
-yarn run client
+yarn electron:build
 ```
 
-#### Compiles and minifies for production
-```
-yarn run build
-```
 
-#### Run your tests
-```
-yarn run test
-```
+## Notes
 
-### Lints and fixes files
-```
-yarn# run lint
-```
-
-### Run your unit tests
-```
-yarn# run test:unit
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+- how handle uws deprecated?
+https://www.npmjs.com/package/uws
+- sort the dependencies. what should be dev?
--- src/background.js
+++ src/background.js
@@ -7,6 +7,14 @@ import {
 } from 'vue-cli-plugin-electron-builder/lib'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
+const dgram = require('dgram');
+var expressApp = require('express')();
+var server = require('http').createServer(expressApp);
+// https://github.com/socketio/socket.io/issues/2109
+var io = require('socket.io')(server, {
+  serveClient: false // do not serve the client file, in that case the brfs loader is not needed
+});
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
@@ -57,6 +65,7 @@ app.on('ready', async () => {
     // Install Vue Devtools
     await installVueDevtools()
   }
+  initUDPSocketServer();
   createWindow()
 })
 
@@ -73,4 +82,80 @@ if (isDevelopment) {
       app.quit()
     })
   }
+}
+
+function initUDPSocketServer () {
+  const updHost = '127.0.0.1';
+  const udpPort = 49000;
+
+  const udpClient = dgram.createSocket('udp4');
+
+  io.on('connection', (socket) => {
+    console.log('connection established');
+    udpClient.on('message', (msg, rinfo) => {
+      const position = readMessage(msg);
+      // console.log('rinfo', rinfo);
+      io.emit('position', position);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('disconnected');
+      io.emit('user disconnected');
+    });
+  });
+
+  udpClient.bind(udpPort, updHost);
+
+  const socketPort = process.env.PORT || 3000;
+
+  server.listen(socketPort);
+}
+
+function readMessage(msg) {
+  // console.log('msg', msg);
+
+  const data = msg.slice(0, 4).toString();
+  // console.log('data', data);
+
+  const index = msg.readInt8(5);
+  // console.log('index', index);
+
+  const values = msg.slice(9);
+  // console.log('values', values);
+
+  const latitude = msg.slice(9, 13).readFloatLE();
+  // console.log('latitude', latitude);
+
+  const longitude = msg.slice(13, 17).readFloatLE();
+  // console.log('longitude', longitude);
+
+  const altitude = msg.slice(17, 21).readFloatLE();
+  // console.log('altitude', altitude);
+
+/*   const value4 = msg.slice(21, 25).readFloatLE();
+  console.log('value4', value4);
+
+  const value5 = msg.slice(21, 25).readFloatLE();
+  console.log('value5', value5);
+
+  const value6 = msg.slice(25, 29).readFloatLE();
+  console.log('value6', value6);
+
+  const value7 = msg.slice(29, 33).readFloatLE();
+  console.log('value7', value7);
+
+  const value8 = msg.slice(33, 37).readFloatLE();
+  console.log('value8', value8); */
+
+  // console.log(latitude + ' | ' + longitude + ' | ' + altitude);
+
+  const result = {
+    latitude,
+    longitude,
+    altitude,
+  };
+
+  // console.log('result', result);
+
+  return result;
 }
