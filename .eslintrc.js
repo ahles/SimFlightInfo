85899857b7cf38af607da5a053c057036e4f2729
Philippe Ahles <philippe@ahles.ch>
2020-08-25 15:47:06 +0200

    adjust eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,32 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: [
     'plugin:vue/essential',
     '@vue/airbnb',
   ],
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
+    'no-debugger': 'off',
     'vue/max-attributes-per-line': 0,
     'no-shadow': ['error', { allow: ['state'] }],
     'max-len': 0,
     'no-plusplus': 0,
-    // 'import/no-unresolved': 0,
-    // 'import/extensions': 0,
     'import/no-extraneous-dependencies': 0,
     'object-shorthand': 0,
   },
+
   parserOptions: {
     parser: 'babel-eslint',
   },
+
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/airbnb'
+  ]
 };
