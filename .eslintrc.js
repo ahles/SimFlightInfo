73e34c44d56fa5646cbba471553b609f9b1aac76
Philippe Ahles <philippe@ahles.ch>
2020-08-31 14:16:35 +0200

    switch linting settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,14 +11,8 @@ module.exports = {
   ],
 
   rules: {
-    'no-console': 'off',
-    'no-debugger': 'off',
-    'vue/max-attributes-per-line': 0,
-    'no-shadow': ['error', { allow: ['state'] }],
-    'max-len': 0,
-    'no-plusplus': 0,
-    'import/no-extraneous-dependencies': 0,
-    'object-shorthand': 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
 
   parserOptions: {
