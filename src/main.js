323cacca583a12524a7c5364502374a8732deb32
Philippe Ahles <philippe@ahles.ch>
2021-02-17 20:19:52 +0100

    no @babel/polyfill

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import vuetify from './plugins/vuetify';
 import App from './App.vue';
 import store from './store';
-import '@babel/polyfill';
 import i18n from './plugins/i18n';
 
 require('../node_modules/leaflet/dist/leaflet.css');
