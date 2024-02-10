6bad0c927c84d28e50a19721be782fce584446db
Philippe Ahles <philippe@ahles.ch>
2024-02-10 12:26:48 +0100

    Cleanup simState.ts

--- src/stores/simState.ts
+++ src/stores/simState.ts
@@ -5,38 +5,31 @@ interface StateInterface {
   center: Coordinate
   zoom: number
   weapon: number
-  markers: Array<MarkerInterface>
   asideCollapsed: boolean
 }
 
-interface MarkerInterface {
-  coordinate: Coordinate
-  weapon: number
-}
-
-export const useUWRStateStore = defineStore({
-  id: 'UWRState',
+export const useSimStateStore = defineStore({
+  id: 'simState',
   // persist: true,
   state: (): StateInterface => ({
     center: [31.392222, 48.999722],
     zoom: 7,
     weapon: 1,
-    markers: [] as Array<MarkerInterface>,
     asideCollapsed: false
   }),
-  actions: {
-    addMarker(marker: MarkerInterface) {
-      this.markers.push(marker)
-    },
-    getMarkers() {
-      const result: Array<MarkerInterface> = []
-      this.markers.forEach((element: MarkerInterface) => {
-        result.push(element)
-      })
-      return result
-    },
-    clearMarkers() {
-      this.markers = []
-    }
-  }
+  // actions: {
+  //   addMarker(marker: MarkerInterface) {
+  //     this.markers.push(marker)
+  //   },
+  //   getMarkers() {
+  //     const result: Array<MarkerInterface> = []
+  //     this.markers.forEach((element: MarkerInterface) => {
+  //       result.push(element)
+  //     })
+  //     return result
+  //   },
+  //   clearMarkers() {
+  //     this.markers = []
+  //   }
+  // }
 })
