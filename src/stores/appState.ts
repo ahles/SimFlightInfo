2a3d5738d9ca735274944913fa1846a101d927ca
Philippe Ahles <philippe@ahles.ch>
2024-02-13 11:25:00 +0100

    Cleanup

--- src/stores/appState.ts
+++ src/stores/appState.ts
@@ -6,32 +6,10 @@ export const useAppStateStore = defineStore({
   state: (): AppStateInterface => ({
     sidePanelOpen: false,
     loading: true,
-    // theme: 'dark',
-    // themeSelectorOpen: false,
-    // themes: [
-    //   {
-    //     label: 'dark',
-    //     value: 'dark'
-    //   },
-    //   {
-    //     label: 'light',
-    //     value: 'light'
-    //   }
-    // ]
   }),
   actions: {
     sidePanelToggle() {
       this.sidePanelOpen = !this.sidePanelOpen
     },
-  //   themeSelectorToggle() {
-  //     this.themeSelectorOpen = !this.themeSelectorOpen
-  //   },
-  //   setTheme(value: string) {
-  //     this.theme = value
-  //     const htmlElement = document.querySelector('html')
-  //     if (htmlElement !== null) {
-  //       htmlElement.dataset.theme = this.theme
-  //     }
-  //   }
   }
 })
