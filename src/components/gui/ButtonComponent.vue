b6f3ac2c8e4f3f12bbb8efd16ac946ecef6a4fec
Philippe Ahles <philippe.ahles@wsl.ch>
2024-02-13 17:26:33 +0100

    Cleanup

--- src/components/gui/ButtonComponent.vue
+++ src/components/gui/ButtonComponent.vue
@@ -12,30 +12,6 @@ defineProps<{
 </template>
 
 <style scoped>
-/* button {
-  cursor: pointer;
-  border: 1px solid var(--color-accent-1);
-  color: white;
-  background-color: var(--color-accent-1);
-  appearance: none;
-  padding: 0.5rem 1rem;
-  border-radius: 3px;
-  line-height: 1;
-  white-space: nowrap;
-
-  &:hover {
-    background-color: white;
-    color: var(--color-accent-1);
-    border-color: var(--color-accent-1);
-    transition: all 0.2s ease-in;
-  }
-
-  svg {
-    width: 100%;
-    height: 100%;
-  }
-} */
-
 .btn {
   appearance: none;
   padding: 0;
