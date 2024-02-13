64e990a1fab055e0c558b54dbeb8341e7e87670e
Philippe Ahles <philippe@ahles.ch>
2024-02-13 10:27:38 +0100

    Disable unused code

--- src/stores/appState.ts
+++ src/stores/appState.ts
@@ -3,34 +3,34 @@ import { defineStore } from 'pinia'
 export const useAppStateStore = defineStore({
   id: 'appState',
   state: () => ({
-    sidePanelOpen: false,
+    // sidePanelOpen: false,
     loading: true,
-    theme: 'dark',
-    themeSelectorOpen: false,
-    themes: [
-      {
-        label: 'dark',
-        value: 'dark'
-      },
-      {
-        label: 'light',
-        value: 'light'
-      }
-    ]
+    // theme: 'dark',
+    // themeSelectorOpen: false,
+    // themes: [
+    //   {
+    //     label: 'dark',
+    //     value: 'dark'
+    //   },
+    //   {
+    //     label: 'light',
+    //     value: 'light'
+    //   }
+    // ]
   }),
-  actions: {
-    sidePanelToggle() {
-      this.sidePanelOpen = !this.sidePanelOpen
-    },
-    themeSelectorToggle() {
-      this.themeSelectorOpen = !this.themeSelectorOpen
-    },
-    setTheme(value: string) {
-      this.theme = value
-      const htmlElement = document.querySelector('html')
-      if (htmlElement !== null) {
-        htmlElement.dataset.theme = this.theme
-      }
-    }
-  }
+  // actions: {
+  //   sidePanelToggle() {
+  //     this.sidePanelOpen = !this.sidePanelOpen
+  //   },
+  //   themeSelectorToggle() {
+  //     this.themeSelectorOpen = !this.themeSelectorOpen
+  //   },
+  //   setTheme(value: string) {
+  //     this.theme = value
+  //     const htmlElement = document.querySelector('html')
+  //     if (htmlElement !== null) {
+  //       htmlElement.dataset.theme = this.theme
+  //     }
+  //   }
+  // }
 })
