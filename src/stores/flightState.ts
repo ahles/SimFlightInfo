28d496e10a343957c8d47b079316d53dae743ad9
Philippe Ahles <philippe@ahles.ch>
2024-02-14 11:08:10 +0100

    Add angle units

--- src/stores/flightState.ts
+++ src/stores/flightState.ts
@@ -8,9 +8,9 @@ export const useFlightStateStore = defineStore({
     longitude: 0,
     altitude: 0,
     altitudeAboveGround: 0,
-    heading: 0,
-    degreesBank: 0,
-    degreesPitch: 0,
+    heading: 0, // degrees
+    degreesBank: 0, // rad
+    degreesPitch: 0, // rad
     airSpeedTrue: 0,
     airSpeedIndicated: 0,
     verticalSpeed: 0
