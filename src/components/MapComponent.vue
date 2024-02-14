c528ba295a3f7f2d5eb277e38a80089fb1c89231
Philippe Ahles <philippe@ahles.ch>
2024-02-14 11:27:37 +0100

    Cleanup

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
-import type Map from 'ol/Map'
+// import type Map from 'ol/Map'
 import PlaneMarkerComponent from './gui/PlaneMarkerComponent.vue'
 import { initMap } from '../modules/map'
 
@@ -8,7 +8,7 @@ import { useAppStateStore } from '../stores/appState'
 
 const appState = useAppStateStore()
 
-let map: Map
+// let map: Map
 
 const props = defineProps<{
   longitude: number,
@@ -17,7 +17,7 @@ const props = defineProps<{
 }>()
 
 onMounted(async () => {
-  map = await initMap(props.longitude, props.latitude)
+  await initMap(props.longitude, props.latitude)
   appState.loading = false
 })
 </script>
