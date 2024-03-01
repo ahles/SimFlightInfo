76aa346d8c17becffb3438c4010dbd6f6e9cc0e6
Philippe Ahles <philippe@ahles.ch>
2024-03-01 18:00:39 +0100

    Add animated background

--- src/components/ConnectionInformationComponent.vue
+++ src/components/ConnectionInformationComponent.vue
@@ -39,14 +39,23 @@ function retry() {
       </div>
     </transition>
   </div>
+  <div class="connection-information__background">
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+  </div>
 </template>
 
 <style scoped>
 .connection-information {
   position: absolute;
+  z-index: 13;
   top: 40%;
   left: 50%;
   transform: translateX(-50%);
+  background: rgba(0, 0, 0, 50%);
+  padding: 2rem;
+   border-radius: 1rem;
 }
 
 .connection-information h2 {
@@ -83,5 +92,79 @@ function retry() {
 
 .connection-information__status--success {
   color: var(--color-success);
+}
+
+/* https://codepen.io/baarbaracrr/pen/KKovmGb */
+.connection-information__background {
+  position: absolute;
+  z-index: 11;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: linear-gradient(315deg, rgba(101,0,94,1) 3%, rgba(60,132,206,1) 38%, rgba(48,238,226,1) 68%, rgba(0,0,255,1) 98%);
+  animation: gradient 15s ease infinite;
+  background-size: 400% 400%;
+  background-attachment: fixed;
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 0%;
+  }
+  50% {
+    background-position: 100% 100%;
+  }
+  100% {
+    background-position: 0% 0%;
+  }
+}
+
+.wave {
+  background: rgb(0 0 0 / 25%);
+  border-radius: 1000% 1000% 0 0;
+  position: fixed;
+  width: 200%;
+  height: 12em;
+  animation: wave 10s -3s linear infinite;
+  transform: translate3d(0, 0, 0);
+  opacity: 0.8;
+  bottom: 0;
+  left: 0;
+  z-index: 12;
+}
+
+.wave:nth-of-type(2) {
+  bottom: -1.25em;
+  animation: wave 18s linear reverse infinite;
+  opacity: 0.8;
+}
+
+.wave:nth-of-type(3) {
+  bottom: -2.5em;
+  animation: wave 20s -1s reverse infinite;
+  opacity: 0.9;
+}
+
+@keyframes wave {
+  2% {
+    transform: translateX(1);
+  }
+
+  25% {
+    transform: translateX(-25%);
+  }
+
+  50% {
+    transform: translateX(-50%);
+  }
+
+  75% {
+    transform: translateX(-25%);
+  }
+
+  100% {
+    transform: translateX(1);
+  }
 }
 </style>
